c1d8c3cc08abf26a18f06ac0a076e8f1d6a95d92
Denner <lucas.denner14@hotmail.com>
2024-11-20 17:35:45 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Celular celularA = new Celular();
+        Celular celularB = new Celular();
+
+        celularA.nome = "xiaomi";
+        celularA.sistemaOperacional = "Android";
+        celularA.tamanhoTela = 6.1f;
+        celularA.espacoArmazenamento = 256;
+
+        celularB.nome = "Iphone 12";
+        celularB.sistemaOperacional = "IOS";
+        celularB.tamanhoTela = 6.1f;
+        celularB.espacoArmazenamento = 256;
+
+        System.out.println("O meu celular é : " + celularA.nome);
+        System.out.println("O sistema operacional é: " + celularA.sistemaOperacional);
+        System.out.println("O tamanho da tela é: " + celularA.tamanhoTela);
+        System.out.println("Tem : " + celularA.espacoArmazenamento + "g de armazenamento");
+
+        System.out.format("O meu celular é um %s, o seu sistema é %s, tem %.1f de tela e %dg de armazenamento" , celularB.nome , celularB.sistemaOperacional , celularB.tamanhoTela , celularB.espacoArmazenamento);
+
+    }
+}
